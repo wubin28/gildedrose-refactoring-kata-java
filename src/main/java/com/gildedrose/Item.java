7d4f3ee776d7903e9e9a358604fdc9f7cd8de0e6
benwu <wubinben@icloud.com>
2020-01-01 21:15:05 +0800

    remove old logic

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -20,77 +20,13 @@ public class Item {
     }
 
     public void update() {
-        if (!name.equals("Aged Brie")
-                && !name.equals("Backstage passes to a TAFKAL80ETC concert")
-                && !name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
+        sellIn = sellIn - 1;
+        if (sellIn < 0) {
             if (quality > 0) {
                 quality = quality - 1;
-            }
-            sellIn = sellIn - 1;
-            if (sellIn < 0) {
-                if (quality > 0) {
-                    quality = quality - 1;
-                }
-            }
-            return;
-        }
-
-        if (!name.equals("Aged Brie")
-                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            // Normal or Sulfuras
-            if (quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    // Normal
-                    quality = quality - 1;
-                }
-            }
-        } else {
-            // Aged Brie or Backstage passes
-            if (quality < 50) {
-                quality = quality + 1;
-
-                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    // Backstage passes
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            // Normal or Aged Brie or Backstage passes
-            sellIn = sellIn - 1;
-        }
-
-        if (sellIn < 0) {
-            if (!name.equals("Aged Brie")) {
-                // Normal or Sulfuras or Backstage passes
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    // Normal or Sulfuras
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            // Normal
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    // Backstage passes
-                    quality = quality - quality;
-                }
-            } else {
-                // Aged Brie
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
             }
         }
     }
