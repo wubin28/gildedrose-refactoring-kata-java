5bcc400eca6ad1573d84c852428e313c9b5e2141
benwu <wubinben@icloud.com>
2020-01-01 19:39:42 +0800

    add comments for item types in each condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,16 +16,20 @@ public class GildedRose {
     private void updateItem(Item item) {
         if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            // Normal or Sulfuras
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    // Normal
                     item.quality = item.quality - 1;
                 }
             }
         } else {
+            // Aged Brie or Backstage passes
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    // Backstage passes
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
@@ -42,21 +46,27 @@ public class GildedRose {
         }
 
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            // Normal or Aged Brie or Backstage passes
             item.sellIn = item.sellIn - 1;
         }
 
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
+                // Normal or Sulfuras or Backstage passes
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    // Normal or Sulfuras
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            // Normal
                             item.quality = item.quality - 1;
                         }
                     }
                 } else {
+                    // Backstage passes
                     item.quality = item.quality - item.quality;
                 }
             } else {
+                // Aged Brie
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
